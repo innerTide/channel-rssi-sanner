b1b6ac796bc6ac9f0a50113d00889bf29ea70cb0
Yuefeng Wu <wyf1116@gmail.com>
2016-10-18 06:41:27 +0800

    Final version, WORKING_SCAN_TIME need to be tested!

--- channel-rssi-scanner.c
+++ channel-rssi-scanner.c
@@ -10,7 +10,7 @@
  * \author
  *         Joakim Eriksson, joakime@sics.se
  *          Adam Dunkels, adam@sics.se
- * 			Yuefeng Wu, y.wu.5@student.tue.nl
+ *         Yuefeng Wu, y.wu.5@student.tue.nl
  */
 
 #include "contiki.h"
@@ -31,33 +31,48 @@
 /*---------------------------------------------------------------------------*/
 /* This is a set of predefined values for this laboratory*/
 
-#define RSSI_THRESHOLD 20
 #define AVERAGE_COUNT 32
-#define TOTAL_DELAY_TIME 1397 /*Unit: uSec*/
+#define WORKING_SCAN_COUNT 16
 
 /*---------------------------------------------------------------------------*/
 /* This is a set of global variables for this laboratory*/
 
-int currentChannel = 26;
-static struct timer scanTimer;
+// int currentChannel = 26;
 
 /*---------------------------------------------------------------------------*/
-
 /* This is a function to calculate average RSSI*/
 int cc2420_average_rssi(){
     int averagedRssi = 0;
     int i = 0;
-    int delayPerLoop = TOTAL_DELAY_TIME/AVERAGE_COUNT;
-    timer_set(&scanTimer, 1);
     while(i < AVERAGE_COUNT){
-        if (timer_expired(&scanTimer)){
-            averagedRssi = averagedRssi + cc2420_rssi();
-            i = i + 1;
-            timer_reset(&scanTimer);
-        }
+        averagedRssi = averagedRssi + cc2420_rssi();
+        i = i + 1;
     }
     return averagedRssi/AVERAGE_COUNT;
 }
+
+/*---------------------------------------------------------------------------*/
+/* This is a function to calculate approximate working RSSI based on daul-scan*/
+int cc2420_working_rssi(){
+    int rssiA = -100;
+    int rssiB = -100;
+    int i,tmpRssi;
+    for (i = 0; i < WORKING_SCAN_COUNT; i++){
+        tmpRssi = cc2420_rssi();
+        if (tmpRssi>rssiB){
+            if (tmpRssi>rssiA){
+                rssiB = rssiA;
+                rssiA = tmpRssi;
+            }
+            else{
+                rssiB = tmpRssi;
+            }
+        }
+    }
+    return (rssiA + rssiB)/2;
+}
+
+
 PROCESS(channel_scanner, "Channel Scanner");
 AUTOSTART_PROCESSES(&channel_scanner);
 
@@ -78,10 +93,15 @@ PROCESS_THREAD(channel_scanner, ev, data)
 	while(1) {
 // 		etimer_set (&etScan, CLOCK_SECOND*30);
 //                 cc2420_set_channel(currentChannel);
-                int rssi = cc2420_average_rssi() + 100;
-                if (rssi != 0){
-                    printf("CH-%d RSSI: %d\n",currentChannel,rssi);
+            int i;
+            for (i = 11; i<27; i++){
+                cc2420_set_channel(i);
+                int rssiAverage = cc2420_average_rssi() + 100;
+                int rssiWorking = cc2420_working_rssi() + 100;
+                if (rssiAverage||rssiWorking){
+                    printf("CH-%d RSSI: %d,[%d]\n",i,rssiAverage,rssiWorking);
                 }
+            }
 	}
 
 	PROCESS_END();
