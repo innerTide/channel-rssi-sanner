1b94c0225532614b3144c134bd7e8d505e131f72
Yuefeng Wu <wyf1116@gmail.com>
2016-10-17 08:01:55 +0800

    update average alg

--- channel-rssi-scanner.c
+++ channel-rssi-scanner.c
@@ -15,7 +15,6 @@
 
 #include "contiki.h"
 #include "net/rime/rime.h"
-#include "sys/etimer.h"
 #include "dev/leds.h"
 #include "net/netstack.h"
 #include "cc2420.h"
@@ -24,27 +23,48 @@
 #include "dev/spi.h"
 #include <stdio.h>
 #include <string.h>
+#include "sys/clock.h"
+#include "sys/timer.h"
+
 
 
 /*---------------------------------------------------------------------------*/
 /* This is a set of predefined values for this laboratory*/
 
 #define RSSI_THRESHOLD 20
+#define AVERAGE_COUNT 32
+#define TOTAL_DELAY_TIME 1397 /*Unit: uSec*/
 
 /*---------------------------------------------------------------------------*/
 /* This is a set of global variables for this laboratory*/
 
 int currentChannel = 26;
-
+static struct timer scanTimer;
 
 /*---------------------------------------------------------------------------*/
+
+/* This is a function to calculate average RSSI*/
+int cc2420_average_rssi(){
+    int averagedRssi = 0;
+    int i = 0;
+    int delayPerLoop = TOTAL_DELAY_TIME/AVERAGE_COUNT;
+    timer_set(&scanTimer, 1);
+    while(i < AVERAGE_COUNT){
+        if (timer_expired(&scanTimer)){
+            averagedRssi = averagedRssi + cc2420_rssi();
+            i = i + 1;
+            timer_reset(&scanTimer);
+        }
+    }
+    return averagedRssi/AVERAGE_COUNT;
+}
 PROCESS(channel_scanner, "Channel Scanner");
 AUTOSTART_PROCESSES(&channel_scanner);
 
 
 PROCESS_THREAD(channel_scanner, ev, data)
 {
-	static struct etimer etScan;
+// 	static struct etimer etScan;
 	
 	
 	PROCESS_BEGIN();
@@ -56,9 +76,9 @@ PROCESS_THREAD(channel_scanner, ev, data)
         
 	
 	while(1) {
-		etimer_set (&etScan, CLOCK_SECOND*30);
-                cc2420_set_channel(currentChannel);
-                int rssi = cc2420_rssi() + 100;
+// 		etimer_set (&etScan, CLOCK_SECOND*30);
+//                 cc2420_set_channel(currentChannel);
+                int rssi = cc2420_average_rssi() + 100;
                 if (rssi != 0){
                     printf("CH-%d RSSI: %d\n",currentChannel,rssi);
                 }
